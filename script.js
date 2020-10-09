12f4c2fcb21bd38ba079db226d9b6ff7ac9d95b6
Malcolm McFadden <malcolmyates34@gmail.com>
2020-10-09 11:31:55 -0400

    finishing homework

---
+++ script.js
@@ -0,0 +1,14 @@
+function myResume () {
+    window.open(href="Images/Resume CODE.pdf", '_blank');
+
+}
+
+function linkedin () {
+    window.open(href="https://www.linkedin.com/in/malcolm-mcfadden-89b98a1b0/", '_blank');
+
+}
+
+function gitHub () {
+    window.open(href="https://github.com/MalcolmYates34", '_blank');
+
+}
